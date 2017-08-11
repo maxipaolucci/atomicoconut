42b743999b2a717ede24dbf3cbf2080dd15e09ab
Maxi Paolucci <maxipaolucci@gmail.com>
2017-08-11 23:18:24 +1200

    adds material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
+import { CustomMaterialDesignModule } from './custom-material-design/custom-material-design.module'
 
 @NgModule({
   declarations: [
@@ -11,7 +15,10 @@ import { AppService } from './app.service';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    BrowserAnimationsModule,
+    HttpModule,
+    FlexLayoutModule,
+    CustomMaterialDesignModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
---
+++ client/src/app/custom-material-design/custom-material-design.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MaterialModule} from '@angular/material';
+
+@NgModule({
+  imports: [CommonModule, MaterialModule],
+  exports: [MaterialModule]
+})
+export class CustomMaterialDesignModule { }
