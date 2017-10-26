cee25212082552c87a18499a0e7016cc48812d44
Maximiliano Paolucci <maximiliano.paolucci@vodafone.com>
2017-10-27 08:55:40 +1300

    always connect to database configured instance in variables env. Removed DATABASE_PROD

--- start.js
+++ start.js
@@ -11,11 +11,7 @@ if (major < 7 || (major === 7 && minor <= 5)) {
 require('dotenv').config({ path: 'variables.env' });
 
 // Connect to our Database and handle an bad connections
-if (process.env.NODE_ENV === 'development') {
-  mongoose.connect(process.env.DATABASE);
-} else if (process.env.NODE_ENV === 'production') {
-  mongoose.connect(process.env.DATABASE_PROD);
-}
+mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
