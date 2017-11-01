3a4edb8d3befb80318112599bfba6196fd5b42c0
Maximiliano Paolucci <maximiliano.paolucci@vodafone.com>
2017-11-02 10:36:52 +1300

    allow add people to the team , just the view

---
+++ client/src/app/modules/teams/components/add-person-to-team-dialog/add-person-to-team-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+@Component({
+  selector: 'app-add-person-to-team-dialog',
+  templateUrl: './add-person-to-team-dialog.component.html',
+  styleUrls: ['./add-person-to-team-dialog.component.scss']
+})
+export class AddPersonToTeamDialogComponent implements OnInit {
+
+  model : any = { email : null };
+
+  constructor(public dialogRef: MatDialogRef<AddPersonToTeamDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
