cd5c579667fa8382cc6189cdd7e62709f42e9f0c
Maxi Paolucci <maxipaolucci@gmail.com>
2017-11-20 21:54:59 +1300

    command to manipulate db

--- data/load-sample-data.js
+++ data/load-sample-data.js
@@ -1,34 +1,54 @@
 require('dotenv').config({ path: __dirname + '/../variables.env' });
 const fs = require('fs');
+const jsonfile = require('jsonfile');
+
+//get the environment
+function specificEnvironment(element) {
+  return element === 'dev' || element === 'test' || element === 'prod';
+}
+const environment = process.argv.find(specificEnvironment) || null;
+if (!environment) {
+  environment = 'DEV';
+} else {
+  environment = environment.toUpperCase();
+}
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 
 // import all of our models - they need to be imported only once
-const Store = require('../models/Store');
-const Review = require('../models/Review');
+const FinancialInfo = require('../models/FinancialInfo');
+const PersonalInfo = require('../models/PersonalInfo');
 const User = require('../models/User');
-
-
-const stores = JSON.parse(fs.readFileSync(__dirname + '/stores.json', 'utf-8'));
-const reviews = JSON.parse(fs.readFileSync(__dirname + '/reviews.json', 'utf-8'));
-const users = JSON.parse(fs.readFileSync(__dirname + '/users.json', 'utf-8'));
+const Team = require('../models/Team');
+const TeamUser = require('../models/Team_User');
 
 async function deleteData() {
   console.log('😢😢 Goodbye Data...');
-  await Store.remove();
-  await Review.remove();
+  await FinancialInfo.remove();
+  await PersonalInfo.remove();
+  await TeamUser.remove();
+  await Team.remove();
   await User.remove();
-  console.log('Data Deleted. To load sample data, run\n\n\t npm run sample\n\n');
+  console.log('Data Deleted. To load sample data, run\n\n\t npm run loadData\n\n');
   process.exit();
 }
 
 async function loadData() {
+  
   try {
-    await Store.insertMany(stores);
-    await Review.insertMany(reviews);
+    const financialinfos = jsonfile.readFileSync(__dirname + '/financialinfos.json');
+    const personalinfos = jsonfile.readFileSync(__dirname + '/personalinfos.json');
+    const users = jsonfile.readFileSync(__dirname + '/users.json');
+    const teams = jsonfile.readFileSync(__dirname + '/teams.json');
+    const teamusers = jsonfile.readFileSync(__dirname + '/teamusers.json');
+  
     await User.insertMany(users);
+    await Team.insertMany(teams);
+    await TeamUser.insertMany(teamusers);
+    await FinancialInfo.insertMany(financialinfos);
+    await PersonalInfo.insertMany(personalinfos);
     console.log('👍👍👍👍👍👍👍👍 Done!');
     process.exit();
   } catch(e) {
@@ -37,8 +57,42 @@ async function loadData() {
     process.exit();
   }
 }
-if (process.argv.includes('--delete')) {
-  deleteData();
-} else {
-  loadData();
+
+async function dumpData() {
+  try {
+    const users = await User.find({}, { __v : false });
+    jsonfile.writeFileSync(__dirname + '/users.json', users);
+
+    const teams = await Team.find({}, { __v : false });
+    jsonfile.writeFileSync(__dirname + '/teams.json', teams);
+
+    const teamusers = await TeamUser.find({}, { __v : false });
+    jsonfile.writeFileSync(__dirname + '/teamusers.json', teamusers);
+
+    const financialinfos = await FinancialInfo.find({}, { __v : false });
+    jsonfile.writeFileSync(__dirname + '/financialinfos.json', financialinfos);
+
+    const personalinfos = await PersonalInfo.find({}, { __v : false });
+    jsonfile.writeFileSync(__dirname + '/personalinfos.json', personalinfos);
+
+    console.log('👍👍👍👍👍👍👍👍 Done!');
+    process.exit();
+  } catch(e) {
+    console.log('\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n');
+    console.log(e);
+    process.exit();
+  }
 }
+
+
+
+if (process.argv.includes('--delete')) {
+  //deleteData();
+} else if (process.argv.includes('--load')) {
+  //loadData();
+} else {
+  //dumpData();
+  
+}
+console.log(process.argv);
+process.exit();
