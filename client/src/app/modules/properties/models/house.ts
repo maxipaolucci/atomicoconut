721889d1626bd33d3d2bc6d787c296ee5a79f861
Maxi Paolucci <maxipaolucci@gmail.com>
2018-02-11 22:46:14 +1300

    house models complete

--- client/src/app/modules/properties/models/house.ts
+++ client/src/app/modules/properties/models/house.ts
@@ -20,11 +20,74 @@ export class House extends Property {
   agent : string; //this must be a contact in a contact persons module like networking
   
 
-  constructor(name : string = null, description : string = null, slug : string = null, admin : User = null, members : User[] = null) {
-    this.name = name;
-    this.slug = slug;
-    this.description = description;
-    this.admin = admin;
-    this.members = members;
+  constructor(type : 'house' = null,
+      address : string = null,
+      createdBy : User = null,
+      landArea : number = 0,
+      floorArea : number = 0,
+      askingPrice : number = 0,
+      askingPriceUnit : string = null,
+      offerPrice : number = 0,
+      offerPriceUnit : string = null,
+      walkAwayPrice : number = 0,
+      walkAwayPriceUnit : string = null,
+      salePrice : number = 0,
+      salePriceUnit : string = null,
+      dateListed : Date = null,
+      reasonForSelling : string = null,
+      marketValue : number = 0,
+      marketValueUnit : string = null,
+      registeredValue : number = 0,
+      registeredValueUnit : string = null,
+      rates : number = 0,
+      ratesUnit : string = null,
+      insurance : number = 0,
+      insuranceUnit : string = null,
+      renovationCost : number = 0,
+      renovationCostUnit : string = null,
+      maintainanceCost : number = 0,
+      maintainanceCostUnit : string = null,
+      description : string = null,
+      otherCost : number = 0,
+      otherCostUnit : string = null,
+      notes : string = null,
+      capitalGrowth : number = 0,
+      bedrooms : number = 0,
+      bathrooms : number = 0,
+      parkingSpaces : number = 0,
+      fenced : boolean = false,
+      rented : boolean = false,
+      rentPrice : number = 0,
+      rentPriceUnit : string= null,
+      rentPricePeriod : 'week' | 'month' = 'week',
+      rentAppraisalDone : boolean = false,
+      vacancy : number = 0,
+      bodyCorporate : number = 0,
+      bodyCorporateUnit : string= null,
+      utilitiesCost : number = 0,
+      utilitiesCostUnit : string= null,
+      agent : string) {
+    
+    super(type, address, createdBy, landArea, floorArea, askingPrice, askingPriceUnit, offerPrice, offerPriceUnit,
+        walkAwayPrice, walkAwayPriceUnit, salePrice, salePriceUnit, dateListed, reasonForSelling, marketValue,
+        marketValueUnit, registeredValue, registeredValueUnit, rates, ratesUnit, insurance, insuranceUnit,
+        renovationCost, renovationCostUnit, maintainanceCost, maintainanceCostUnit, description, otherCost,
+        otherCostUnit, notes, capitalGrowth);
+
+    this.bedrooms = bedrooms;
+    this.bathrooms = bathrooms;
+    this.parkingSpaces = parkingSpaces;
+    this.fenced = fenced;
+    this.rented = rented;
+    this.rentPrice = rentPrice;
+    this.rentPriceUnit = rentPriceUnit;
+    this.rentPricePeriod = rentPricePeriod;
+    this.rentAppraisalDone = rentAppraisalDone;
+    this.vacancy = vacancy;
+    this.bodyCorporate = bodyCorporate;
+    this.bodyCorporateUnit = bodyCorporateUnit;
+    this.utilitiesCost = utilitiesCost;
+    this.utilitiesCostUnit = utilitiesCostUnit;
+    this.agent = agent;
   }
 }
--- client/src/app/modules/properties/models/property.ts
+++ client/src/app/modules/properties/models/property.ts
@@ -35,11 +35,70 @@ export class Property {
   notes : string;
   capitalGrowth : number;
 
-  constructor(name : string = null, description : string = null, slug : string = null, admin : User = null, members : User[] = null) {
-    this.name = name;
-    this.slug = slug;
+  constructor(type : 'house'= null,
+      address : string = null,
+      createdBy : User = null,
+      landArea : number = 0,
+      floorArea : number = 0,
+      askingPrice : number = 0,
+      askingPriceUnit : string = null,
+      offerPrice : number = 0,
+      offerPriceUnit : string = null,
+      walkAwayPrice : number = 0,
+      walkAwayPriceUnit : string = null,
+      salePrice : number = 0,
+      salePriceUnit : string = null,
+      dateListed : Date = null,
+      reasonForSelling : string = null,
+      marketValue : number = 0,
+      marketValueUnit : string = null,
+      registeredValue : number = 0,
+      registeredValueUnit : string = null,
+      rates : number = 0,
+      ratesUnit : string = null,
+      insurance : number = 0,
+      insuranceUnit : string = null,
+      renovationCost : number = 0,
+      renovationCostUnit : string = null,
+      maintainanceCost : number = 0,
+      maintainanceCostUnit : string = null,
+      description : string = null,
+      otherCost : number = 0,
+      otherCostUnit : string = null,
+      notes : string = null,
+      capitalGrowth : number = 0) {
+    
+    this.type = type;
+    this.address = address;
+    this.createdBy = createdBy;
+    this.landArea = landArea;
+    this.floorArea = floorArea;
+    this.askingPrice = askingPrice;
+    this.askingPriceUnit = askingPriceUnit;
+    this.offerPrice = offerPrice;
+    this.offerPriceUnit = offerPriceUnit;
+    this.walkAwayPrice = walkAwayPrice;
+    this.walkAwayPriceUnit = walkAwayPriceUnit;
+    this.salePrice = salePrice;
+    this.salePriceUnit = salePriceUnit;
+    this.dateListed = dateListed;
+    this.reasonForSelling = reasonForSelling;
+    this.marketValue = marketValue;
+    this.marketValueUnit = marketValueUnit;
+    this.registeredValue = registeredValue;
+    this.registeredValueUnit = registeredValueUnit;
+    this.rates = rates;
+    this.ratesUnit = ratesUnit;
+    this.insurance = insurance;
+    this.insuranceUnit = insuranceUnit;
+    this.renovationCost = renovationCost;
+    this.renovationCostUnit = renovationCostUnit;
+    this.maintainanceCost = maintainanceCost;
+    this.maintainanceCostUnit = maintainanceCostUnit;
     this.description = description;
-    this.admin = admin;
-    this.members = members;
+    this.otherCost = otherCost;
+    this.otherCostUnit = otherCostUnit;
+    this.notes = notes;
+    this.capitalGrowth = capitalGrowth;
   }
 }
