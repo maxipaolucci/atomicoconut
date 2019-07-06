0f391b066f302e62fc6acb1b9d9d9e9155dc1875
Maxi Paolucci mac <maxipaolucci@gmail.com>
2019-07-06 12:25:27 +1200

    different images tags per branch

--- manage_images.sh
+++ manage_images.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# docker build -t $DOCKER_ID/atomic-coconut-client:$TRAVIS_BRANCH ./client
-# docker build -t $DOCKER_ID/atomic-coconut-nginx:$TRAVIS_BRANCH ./nginx
-# docker build -t $DOCKER_ID/atomic-coconut-server:$TRAVIS_BRANCH ./server
+IMAGE_TAG = 'latest'
+if ["$TRAVIS_BRANCH" != "master"]; then
+  IMAGE_TAG = $TRAVIS_BRANCH
+fi
 
-# if [ "$TRAVIS_BRANCH" == "devops" ]; then
-#   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
-#   docker push $DOCKER_ID/atomic-coconut-client:$TRAVIS_BRANCH
-#   docker push $DOCKER_ID/atomic-coconut-nginx:$TRAVIS_BRANCH
-#   docker push $DOCKER_ID/atomic-coconut-server:$TRAVIS_BRANCH
-# fi
+echo "Current branch: $TRAVIS_BRANCH -- Images tag: $IMAGE_TAG"
 
-sed -i "s?atomic-coconut-client?atomic-coconut-client:$TRAVIS_BRANCH?" Dockerrun.aws.json
-sed -i "s?atomic-coconut-server?atomic-coconut-server:$TRAVIS_BRANCH?" Dockerrun.aws.json
-sed -i "s?atomic-coconut-nginx?atomic-coconut-nginx:$TRAVIS_BRANCH?" Dockerrun.aws.json
+docker build -t $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG ./client
+docker build -t $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG ./nginx
+docker build -t $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG ./server
 
-cat Dockerrun.aws.json
+if [ "$TRAVIS_BRANCH" == "devops" ]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+  docker push $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG
+  docker push $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG
+  docker push $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG
+
+  # replace the image names with this one with the current branch as tag
+  sed -i "s?atomic-coconut-client?atomic-coconut-client:$IMAGE_TAG?" Dockerrun.aws.json
+  sed -i "s?atomic-coconut-server?atomic-coconut-server:$IMAGE_TAG?" Dockerrun.aws.json
+  sed -i "s?atomic-coconut-nginx?atomic-coconut-nginx:$IMAGE_TAG?" Dockerrun.aws.json
+fi
