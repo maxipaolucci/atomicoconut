06d624115470c5db936c45b4cd108e6dc1d43d0f
Maxi Paolucci mac <maxipaolucci@gmail.com>
2019-10-29 22:18:17 +1300

    [testing-build] setup pusher api against test app

--- client/src/environments/environment.test.ts
+++ client/src/environments/environment.test.ts
@@ -9,7 +9,7 @@ export const environment = {
   showLogs : false,
   mapsApiKey : 'AIzaSyBVl8QaQnQGBw6IyvlQ5K3XepPj6Z9nHt0',
   pusher: {
-    key: '270a7732c2107a9c36ba',
+    key: '27cd80ad4160d1834d8b',
     cluster: 'ap4',
     channel: 'pusher-events-channel'
   }
