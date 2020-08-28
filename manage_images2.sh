995afcdb489bc0aab3386606b783e583e12cf313
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-08-28 23:01:21 +1200

    [codebuild] adds manage images script

--- manage_images2.sh
+++ manage_images2.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-CURRENT_BRANCH=$(basename $CODEBUILD_WEBHOOK_TRIGGER)
-echo $CURRENT_BRANCH
-git status
 
 IMAGE_TAG='latest'
 DOCKERFILE_CLIENT='Dockerfile'
@@ -20,12 +17,12 @@ echo "Current branch: $CURRENT_BRANCH -- Images tag: $IMAGE_TAG -- Dockerfile cl
 echo
 
 if [[ "$CURRENT_BRANCH" == "master" || "$CURRENT_BRANCH" == "testing" ]]; then
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+
   docker build -t $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG -f ./client/$DOCKERFILE_CLIENT ./client
   docker build -t $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG -f ./nginx/$DOCKERFILE_NGINX ./nginx
   docker build -t $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG ./server
 
-
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
   docker push $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG
   docker push $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG
   docker push $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG
