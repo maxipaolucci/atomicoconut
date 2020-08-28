c8f2852701b5be3ffb2092bb4ed11db6e189b9c8
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-08-28 23:11:27 +1200

    [codebuild] adds param stores

--- manage_images2.sh
+++ manage_images2.sh
@@ -16,22 +16,22 @@ echo
 echo "Current branch: $CURRENT_BRANCH -- Images tag: $IMAGE_TAG -- Dockerfile client: $DOCKERFILE_CLIENT"
 echo
 
-if [[ "$CURRENT_BRANCH" == "master" || "$CURRENT_BRANCH" == "testing" ]]; then
+if [[ "$CURRENT_BRANCH" == "master" || "$CURRENT_BRANCH" == "codebuild" ]]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 
-  docker build -t $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG -f ./client/$DOCKERFILE_CLIENT ./client
+  # docker build -t $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG -f ./client/$DOCKERFILE_CLIENT ./client
   docker build -t $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG -f ./nginx/$DOCKERFILE_NGINX ./nginx
-  docker build -t $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG ./server
+  # docker build -t $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG ./server
 
-  docker push $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG
+  # docker push $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG
   docker push $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG
-  docker push $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG
+  # docker push $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG
 
   # replace the image names with this one with the current branch as tag
   sed -i "s?atomic-coconut-client?atomic-coconut-client:$IMAGE_TAG?" Dockerrun.aws.json
   sed -i "s?atomic-coconut-server?atomic-coconut-server:$IMAGE_TAG?" Dockerrun.aws.json
   sed -i "s?atomic-coconut-nginx?atomic-coconut-nginx:$IMAGE_TAG?" Dockerrun.aws.json
-  git commit -am "updated images tags in Dockerrun.aws.json by Travis job in manage_images.sh"
+  git commit -am "updated images tags in Dockerrun.aws.json by CI build job in manage_images.sh"
 else
   echo
   echo "Image push is skipped in branch '$CURRENT_BRANCH'. Only 'master' and 'testing' branches push images to Dockerhub"
