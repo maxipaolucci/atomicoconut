727d046c858b5f877e57297764a82b78c23f0b41
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-09-17 14:32:50 +1200

    [testing] commented out all docker commands for testing faster

--- manage_images.sh
+++ manage_images.sh
@@ -17,15 +17,15 @@ echo "Current branch: $CURRENT_BRANCH -- Images tag: $IMAGE_TAG -- Dockerfile cl
 echo
 
 if [[ "$CURRENT_BRANCH" == "master" || "$CURRENT_BRANCH" == "testing" ]]; then
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
+  # echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
   
-  docker build -t $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG -f ./client/$DOCKERFILE_CLIENT ./client
-  docker build -t $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG -f ./nginx/$DOCKERFILE_NGINX ./nginx
-  docker build -t $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG ./server
+  # docker build -t $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG -f ./client/$DOCKERFILE_CLIENT ./client
+  # docker build -t $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG -f ./nginx/$DOCKERFILE_NGINX ./nginx
+  # docker build -t $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG ./server
 
-  docker push $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG
-  docker push $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG
-  docker push $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG
+  # docker push $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG
+  # docker push $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG
+  # docker push $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG
 
   # replace the image names with this one with the current branch as tag
   sed -i "s?atomic-coconut-client?atomic-coconut-client:$IMAGE_TAG?" Dockerrun.aws.json
