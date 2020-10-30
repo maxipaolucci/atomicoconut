e4ef53189de7ceda77bf29c6a76513bc150c1003
Maxi Paolucci mac <maxipaolucci@gmail.com>
2020-10-30 16:14:33 +1300

    [aws-linux-2] fix logs group and logs file name for amazon cloudwatch

--- .platform/hooks/postdeploy/01-setup-cloudwatch-agent.sh
+++ .platform/hooks/postdeploy/01-setup-cloudwatch-agent.sh
@@ -4,6 +4,8 @@ echo "Setting up Unified CloudWatch Agent..."
 
 FILE_DIR="/home/ec2-user"
 FILE="$FILE_DIR/cloudWatchAgentConfig-updated.json"
+LOGS_FILE="/var/log/eb-docker/containers/eb-current-app/eb-stdouterr.log"
+
 if [ -f $FILE ]; then
   echo "$FILE removed."
   rm $FILE
@@ -16,23 +18,32 @@ cp $FILE_DIR/cloudWatchAgentConfig.json $FILE
 ls -la $FILE
 echo
 
-SERVER_CONTAINER_ID=$(docker ps | grep atomic-coconut-server | awk '{print $1}')
-if [[ -z "$SERVER_CONTAINER_ID" ]]; then
-  rm $FILE
-  echo "[WARN] aCo server container not found. Exit cloudwatch Agent setup with no changes."
-  exit 0;
-fi
-echo "aCo server container id: $SERVER_CONTAINER_ID"
+# SERVER_CONTAINER_ID=$(docker ps | grep atomic-coconut-server | awk '{print $1}')
+# if [[ -z "$SERVER_CONTAINER_ID" ]]; then
+#   rm $FILE
+#   echo "[WARN] aCo server container not found. Exit cloudwatch Agent setup with no changes."
+#   exit 0;
+# fi
+# echo "aCo server container id: $SERVER_CONTAINER_ID"
 
 ENVIRONMENT_TYPE=$(sudo /opt/elasticbeanstalk/bin/get-config environment | jq .ENVIRONMENT_TYPE | tr -d '"')
 if [[ -z "$ENVIRONMENT_TYPE" ]]; then ENVIRONMENT_TYPE=production; fi
 echo "Environment type: $ENVIRONMENT_TYPE"
 
-sed -i "s?server-CONTAINER_ID-stdouterr?server-$SERVER_CONTAINER_ID-stdouterr?" $FILE
+# sed -i "s?server-CONTAINER_ID-stdouterr?server-$SERVER_CONTAINER_ID-stdouterr?" $FILE
 sed -i "s?aco-ENVIRONMENT_TYPE?aco-$ENVIRONMENT_TYPE?" $FILE
 echo
 
 echo "Setup cloudWatch agent..."
+
+if [ ! -f $LOGS_FILE ]; then
+  echo "[WARN] $LOGS_FILE not found. Exit cloudwatch Agent setup."
+  exit 0;
+else
+  echo "Using logs from: $LOGS_FILE"
+  echo
+fi
+
 if [ ! -f /usr/share/collectd/types.db ]; then
   echo "Created file: /usr/share/collectd/types.db required by CloudWatch Agent."
   sudo mkdir -p /usr/share/collectd
