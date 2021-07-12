49a5935ea63c7063097c282ecbf19a943f4dcf0a
Maxi Paolucci mac <maxipaolucci@gmail.com>
2021-07-12 19:15:21 +1200

    [elk] integration for production and testing logs

--- aws/cronjob.sh
+++ aws/cronjob.sh
@@ -5,7 +5,8 @@ dt=`date '+%d/%m/%Y_%H:%M:%S'`
 echo "Importing files from S3 at $dt..." >> /var/log/cronjobs.log
 
 # not using the $S3_BUCKET env variable because cronjob has issues loading them
-/usr/local/bin/aws s3 sync s3://elasticbeanstalk-ap-southeast-2-782522910439/atomiCoconut/logs /aws/s3-logs >> /var/log/cronjobs.log
+/usr/local/bin/aws s3 sync s3://elasticbeanstalk-ap-southeast-2-782522910439/atomiCoconut/logs /aws/s3-logs/production >> /var/log/cronjobs.log
+/usr/local/bin/aws s3 sync s3://elasticbeanstalk-ap-southeast-2-782522910439/atomiCoconut-testing/logs /aws/s3-logs/testing >> /var/log/cronjobs.log
 
 echo "Files imported successfully." >> /var/log/cronjobs.log
 echo " " >> /var/log/cronjobs.log
