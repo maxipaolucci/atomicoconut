fd556f286780bf51583fe5d417b6e83129ff4c4b
Maxi Paolucci mac <maxipaolucci@gmail.com>
2021-07-17 18:06:00 +1200

    [elk] set chmod 644 for logrotate file. removes filebeat image from manage images script

--- manage_images.sh
+++ manage_images.sh
@@ -22,18 +22,16 @@ if [[ "$CURRENT_BRANCH" == "master" || "$CURRENT_BRANCH" == "testing" ]]; then
   docker build -t $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG -f ./client/$DOCKERFILE_CLIENT ./client
   docker build -t $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG -f ./nginx/$DOCKERFILE_NGINX ./nginx
   docker build -t $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG ./server
-  docker build -t $DOCKER_ID/atomic-coconut-filebeat:$IMAGE_TAG -f ./elk/Dockerfile.filebeat ./elk
-
+  
   docker push $DOCKER_ID/atomic-coconut-client:$IMAGE_TAG
   docker push $DOCKER_ID/atomic-coconut-nginx:$IMAGE_TAG
   docker push $DOCKER_ID/atomic-coconut-server:$IMAGE_TAG
-  docker push $DOCKER_ID/atomic-coconut-filebeat:$IMAGE_TAG
-
+  
   # replace the image names with this one with the current branch as tag
   sed -i "s?atomic-coconut-client?atomic-coconut-client:$IMAGE_TAG?" docker-compose-beanstalk.yml
   sed -i "s?atomic-coconut-server?atomic-coconut-server:$IMAGE_TAG?" docker-compose-beanstalk.yml
   sed -i "s?atomic-coconut-nginx?atomic-coconut-nginx:$IMAGE_TAG?" docker-compose-beanstalk.yml
-  sed -i "s?atomic-coconut-filebeat?atomic-coconut-filebeat:$IMAGE_TAG?" docker-compose-beanstalk.yml
+
 else
   echo
   echo "Image push is skipped in branch '$CURRENT_BRANCH'. Only 'master' and 'testing' branches push images to Dockerhub"
